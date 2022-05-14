b0d6bdef43c9cfabc14d577321a936324c7da063
sodiqchinko <sodiqridwan100@gmail.com>
2022-05-14 20:27:38 +0100

    break test and save video

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p id={"web-title"}>Cypress Demo Webapp</p>
+        <p id={"web-title"}>Demo Webapp</p>
         <a
           className="App-link"
           href="https://reactjs.org"
